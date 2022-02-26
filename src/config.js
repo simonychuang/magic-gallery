c1ef6a76b01c987c216910f6a3a4e6688625d94a
simonychuang <simonhuangyc@gmail.com>
2022-02-26 13:52:59 -0500

    Removed axios and dotenv and use mtgsdk instead

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const dev = {
-  REACT_APP_BASE_ENDPOINT_URL: process.env.REACT_APP_BASE_ENDPOINT_URL,
-}
-
-const prod = {
-  REACT_APP_BASE_ENDPOINT_URL: process.env.REACT_APP_BASE_ENDPOINT_URL,
-}
-
-export const config = process.env.REACT_APP_BUILD_ENV === 'production' ? prod : dev;
